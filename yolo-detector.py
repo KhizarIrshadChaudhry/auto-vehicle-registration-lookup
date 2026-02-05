b3849db40b550c2be20e42da92192b03b6231a35
Khizar Irshad Chaudhry <k.irshadch@gmail.com>
2026-02-05 09:50:19 +0100

    omformulated to OOP object

--- yolo-detector.py
+++ yolo-detector.py
@@ -7,15 +7,59 @@ import easyocr
 # Retrieved 2026-02-02, License - CC BY-SA 4.0
 
 import ssl
-ssl._create_default_https_context = ssl._create_stdlib_context
 
+
+
+
+class ANPRDetector:
+    def __init__(self, model_path='license_plate_detector.pt', cap=1):
+        ssl._create_default_https_context = ssl._create_stdlib_context
+
+        self.model_path = model_path
+        self.cap = cv2.VideoCapture(cap)
+        self.model = YOLO(self.model_path)
+        self.reader = easyocr.Reader(['en']) # OCR
+
+        self.detect_and_read()
+
+    def detect_and_read(self):
+        reader = easyocr.Reader(['en'])
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+
+            results = self.model(frame)
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    license_plate_crop = frame[y1:y2, x1:x2]
+                    plate_text = reader.readtext(license_plate_crop, detail=0)
+
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.putText(frame, str(plate_text), (x1, y1-10), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+
+            cv2.imshow('ANPR', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    detector = ANPRDetector(cap="video.mp4")
+
+
+'''
 # 1. Load YOLOv8 Model
 #https://github.com/Muhammad-Zeerak-Khan/Automatic-License-Plate-Recognition-using-YOLOv8/blob/main/license_plate_detector.pt
 model = YOLO('license_plate_detector.pt') # Path to your custom trained model
 reader = easyocr.Reader(['en'])
 
 # 2. Capture Video/Image
-cap = cv2.VideoCapture('video.mp4')
+cap = cv2.VideoCapture(1)
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -31,15 +75,17 @@ while cap.isOpened():
             
             # 4. Crop and Read
             license_plate_crop = frame[y1:y2, x1:x2]
-            plate_text = reader.readtext(license_plate_crop, detail=0)
+            #plate_text = reader.readtext(license_plate_crop, detail=0)
             
             # 5. Draw
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, str(plate_text), (x1, y1-10), 
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            #cv2.putText(frame, str(plate_text), (x1, y1-10), 
+            #            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
 
     cv2.imshow('ANPR', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 cap.release()
 cv2.destroyAllWindows()
+'''
+
